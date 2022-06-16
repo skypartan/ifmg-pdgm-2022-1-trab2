8fddd01a187ba75d1f14762c212aa11c04f81146
skypartan <lucasgb@skypartan.net>
2022-06-16 19:45:38 -0300

    Especificando bibliotecas

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    someFunc
+) where
+
+import Data.Conduit
+import Data.Conduit.Binary
+import Data.Conduit.List as CL
+import Data.CSV.Conduit
+import Data.Text (Text)
+
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- someFunc = putStrLn "someFunc"
+someFunc = test
+
+
+-- Just reverse te columns
+-- myProcessor :: Monad m => Conduit (Row Text) m (Row Text)
+-- myProcessor = CL.map reverse
+
+-- test :: IO ()
+-- test = runResourceT $ 
+--   transformCSV defCSVSettings 
+--                (sourceFile "input.csv") 
+--                myProcessor
+--                (sinkFile "output.csv")
+
+-- test = defCSVSettings (sourceFile "input.csv")
