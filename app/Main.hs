fb355c4fc287f1b468398d311db413d35cab4e39
Konomaster <thomasdovaletec@gmail.com>
2022-06-17 13:53:59 -0300

    loop

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,20 @@ module Main where
 
 import Lib
 
+
 main :: IO()
-main = someFunc
+main = do 
+       someFunc
+       menu
+
+
+menu :: IO ()
+menu = do
+       putStrLn ("Digite uma opcao: ")
+       op <- getLine
+       let action | op /= "sair" = menu
+                  | otherwise = putStrLn ("")
+       action
+
+
+
