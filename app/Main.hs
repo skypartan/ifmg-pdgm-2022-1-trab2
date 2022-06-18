3225e19eb60ff0cf293bc37cd15ab94b0634a081
Konomaster <thomasdovaletec@gmail.com>
2022-06-18 16:53:33 -0300

    salvando meu codigo

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,68 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+
 module Main where
 
-import Lib
+import Data.Csv (FromNamedRecord)
+import Data.Text (Text)
+import Data.Map (Map)
+import Data.Vector (Vector)
+import GHC.Generics (Generic)
+
+import qualified Data.ByteString.Lazy as ByteString
+import qualified Data.Csv             as Csv
+import qualified Data.Vector          as Vector
+
+data Compra = Compra
+   { dia :: Text
+    ,produto :: Text
+    ,loja :: Text
+    ,quantidade :: Integer
+    ,unidade :: Text
+    ,preco :: Double
+    ,total :: Double
+   } deriving (Generic, FromNamedRecord, Show)
+
+teste (x:xs) = do 
+               print ("***" ++ show x ++ "***")
+               teste xs
+teste [] = print ""
+
+
+--menu :: IO ()
+menu vec = do
+       print ("Digite uma opcao: ")
+       op <- getLine
+       let action | op == "sair" = print ""
+                  | op == "1" = do 
+                                  print "OPCAO1" 
+                                  prod <- getLine
+                                  --filter ( \compra -> produto compra == prod ) vec
+                                  menu vec
+                  | otherwise = menu vec 
+       action
 
 
 main :: IO()
 main = do 
-       someFunc
-       menu
+       bytes <- ByteString.readFile "./input.csv"
+
+       case Csv.decodeByName bytes of
+           Left string           -> fail string
+           Right (_header, rows) -> do
+--                 print (rows)
+--                 print (rows :: Vector (Map Text Text))
+--                 print ((fmap loja rows))
+--                 teste (Vector.toList rows)
+                 print (rows :: Vector Compra)
+                 print "//"
+
+                 menu rows
+                                  
 
 
-menu :: IO ()
-menu = do
-       putStrLn ("Digite uma opcao: ")
-       op <- getLine
-       let action | op /= "sair" = menu
-                  | otherwise = putStrLn ("")
-       action
+
 
 
 
