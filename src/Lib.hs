97000c68bd5d35ed8c07476e9451bd4cc9721d61
Konomaster <thomasdovaletec@gmail.com>
2022-06-19 11:15:37 -0300

    so falta a opcao de gerar o relatorio

--- src/Lib.hs
+++ src/Lib.hs
@@ -29,7 +29,7 @@ data Compra = Compra
   deriving (Generic, Show, FromNamedRecord)
 
 
-opcao1 vec = do
+opcao1 vec maux = do
   print "OPCAO1"
   prod <- getLine
   
@@ -40,12 +40,12 @@ opcao1 vec = do
   
   print comprado
 
-  menu vec
+  menu vec maux
 
 calculaTotal (x:xs) = x + calculaTotal xs 
 calculaTotal [] = 0
 
-opcao2 vec = do
+opcao2 vec maux = do
   print "OPCAO2"
   prod <- getLine
   
@@ -56,12 +56,12 @@ opcao2 vec = do
            | otherwise = " unidades"
 
   print ("Quantidade comprada do produto: " ++ show (calculaTotal (V.toList soQtd)) ++ unid)
-  menu vec
+  menu vec maux
 
 
 formatFloatN floatNum numOfDecimals = showFFloat (Just numOfDecimals) floatNum ""
 
-opcao3 vec = do
+opcao3 vec maux = do
   print "OPCAO3"
   prod <- getLine
   
@@ -70,7 +70,7 @@ opcao3 vec = do
 
 
   print ("Valor total comprado do produto: R$ " ++ (formatFloatN (calculaTotal (V.toList soQtd)) 2))
-  menu vec
+  menu vec maux
 
 
 mProdutos (x:[]) = do
@@ -85,7 +85,7 @@ mostraProdutos (x:xs) d = do
 mostraProdutos [] d = do
                   print ("Nada foi comprado em " ++ d)
 
-opcao4 vec = do
+opcao4 vec maux = do
   print "OPCAO4"
   prod <- getLine
   
@@ -96,10 +96,10 @@ opcao4 vec = do
   mostraProdutos (nub (V.toList soProdutos)) prod
   --mostraProdutos (V.toList soProdutos) prod
                       
-  menu vec
+  menu vec maux
 
 
-opcao5 vec = do
+opcao5 vec maux = do
   print "OPCAO5"
   prod <- getLine
   
@@ -107,31 +107,69 @@ opcao5 vec = do
   let soTotal = fmap total result
  
   print ("Quantidade comprada na loja '" ++ prod ++ "': R$ " ++ (formatFloatN (calculaTotal (V.toList soTotal)) 2))                   
-  menu vec
+  menu vec maux
 
 
+inverteL (x:xs) aux = inverteL xs (x:aux)
+inverteL [] aux = aux
 
-opcao6 vec = do
-  print "OPCAO6"
+inverteLista (x:xs) = inverteL (x:xs) []
+inverteLista [] = []
+
+calcProdutos (x:xs) vec aux =
+  let soX = V.filter (\compra -> produto compra == x) vec
+      totaisdoX = fmap quantidade soX
+  in calcProdutos xs vec ((calculaTotal (V.toList totaisdoX)):aux)
+
+calcProdutos [] _ aux = inverteLista aux
  
-  print (nub (V.toList (fmap produto vec)))
 
-  menu vec
+pegaMaisComprados (x:xs) (y:ys) maior items 
+  | y > maior = pegaMaisComprados xs ys y (x:[]) 
+  | y == maior = pegaMaisComprados xs ys y (x:items)
+  | otherwise = pegaMaisComprados xs ys maior items
+
+pegaMaisComprados [] [] _ items = items
+
+pProds [] = print ""
+pProds (x:[]) = do
+  print x
+pProds (x:xs) = do
+  print x
+  pProds xs
+
+printProds (x:xs) = do
+  let prePrint | length (x:xs) == 0 = "Nao foi comprado nenhum produto ainda"
+               | length (x:xs) == 1 = "O produto mais comprado foi: "
+               | length (x:xs) > 1 = "Houve empate. Os produtos mais comprados foram: "
+  print prePrint
+  pProds (x:xs)
+
+
+opcao6 vec maux = do
+  print "OPCAO6"
+  
+  let produtosDistintos = (nub (V.toList (fmap produto vec)))
+      qtdTotalPorProduto = calcProdutos produtosDistintos vec []
+  
+  printProds $ pegaMaisComprados produtosDistintos qtdTotalPorProduto 0 []
+
+  menu vec maux
 
 --menu :: IO ()
-menu vec = do
+menu vec maux = do
   print "Digite uma opcao: "
 
   op <- getLine
   let action
-        | op == "sair" = print ""
-        | op == "1" = opcao1 vec
-        | op == "2" = opcao2 vec
-        | op == "3" = opcao3 vec
-        | op == "4" = opcao4 vec
-        | op == "5" = opcao5 vec
-        | op == "6" = opcao6 vec
-        | otherwise = menu vec
+        | op == "sair" = print "Finalizado com sucesso"
+        | op == "1" = opcao1 vec maux
+        | op == "2" = opcao2 vec maux
+        | op == "3" = opcao3 vec maux
+        | op == "4" = opcao4 vec maux
+        | op == "5" = opcao5 vec maux
+        | op == "6" = opcao6 vec maux
+        | otherwise = menu vec maux 
   
   action
 
@@ -145,7 +183,7 @@ main = do
     Right (_header, rows) -> do
       print (rows :: Vector Compra)
       print "\n\n"
-      menu rows
+      menu rows []
 
 -- nos prints colocar o nome do produto tb?
 -- colocar prints depois dos resultados pra ter um espacinho do menu?
